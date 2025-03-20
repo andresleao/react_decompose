4de0aaf629bf9fdcb4a1b59c9fa7f1819ad0cb38
André Leão <andrefelipesl123@gmail.com>
2025-03-20 16:33:11 -0300

    Components with tests created

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,20 @@
-// import a css file containig header styles
+import './Header.css';
 
-// Create a Header function returning the HTML of header block
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <a href="#about" className="navigation__link">
+        About
+      </a>
+      <a href="#services" className="navigation__link">
+        Services
+      </a>
+      <a href="#contact" className="navigation__link">
+        Contact
+      </a>
+    </header>
+  );
+}
 
-// Add a default export statement for Header component to use it in the other files
+export default Header;
